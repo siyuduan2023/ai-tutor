dfaef70f1b482aaf8ad12d6ced777e712711371e
siyu.duan <siyu.duan@harperjames.co.uk>
2025-09-18 16:28:42 +0000

    learn signal set() and update()

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,10 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {RecipeModel} from './models';
+import { MOCK_RECIPES } from './mock-recipes';
 
 @Component({
   selector: 'app-root',
@@ -15,5 +17,32 @@ import {RouterOutlet} from '@angular/router';
   styleUrl: './app.css',
 })
 export class App {
-  protected title = 'myapp';
+  // protected title = 'myapp';
+  protected readonly title = signal('My Recipe Box');
+
+  protected recipe = signal<RecipeModel>({} as RecipeModel);
+  protected serving = signal(1);
+
+  protected clickButton1() {
+    console.log('Button 1 clicked');
+    const spaghetti = MOCK_RECIPES.find(recipe => recipe.id === 1);
+    if (spaghetti) {
+      this.recipe.set(spaghetti);
+    }
+  }
+
+  protected clickButton2() {
+    console.log('Button 2 clicked');
+    this.recipe.set(MOCK_RECIPES[1]);
+  }
+
+  protected increase(){
+    this.serving.update(serving => serving + 1);
+  }
+
+  protected decrease(){
+    this.serving.update(serving => serving - 1);
+  }
+
+  
 }
