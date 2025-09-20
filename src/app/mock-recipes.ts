778535870d143266a305d347f5db3716668fff3c
siyu.duan <siyu.duan@harperjames.co.uk>
2025-09-20 19:44:08 +0000

    added real image for both recipes

--- src/app/mock-recipes.ts
+++ src/app/mock-recipes.ts
@@ -5,7 +5,7 @@ export const MOCK_RECIPES: RecipeModel[] = [
     id: 1,
     name: 'Spaghetti Carbonara',
     description: 'A classic Italian pasta dish.',
-    imgUrl: 'https://placehold.co/300x200?text=Spaghetti+Carbonara',
+    imgUrl: 'https://images.unsplash.com/photo-1633337474564-1d9478ca4e2e?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     ingredients: [
       { name: 'Spaghetti', quantity: 200, unit: 'g' },
       { name: 'Guanciale', quantity: 100, unit: 'g' },
@@ -18,7 +18,7 @@ export const MOCK_RECIPES: RecipeModel[] = [
     id: 2,
     name: 'Caprese Salad',
     description: 'A simple and refreshing Italian salad.',
-    imgUrl: 'https://placehold.co/300x200?text=Caprese+Salad',
+    imgUrl: 'https://plus.unsplash.com/premium_photo-1677619680535-c3d453823b9e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     ingredients: [
       { name: 'Tomatoes', quantity: 4, unit: 'each' },
       { name: 'Fresh Mozzarella', quantity: 200, unit: 'g' },
