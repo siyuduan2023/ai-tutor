8ecbfc2fde4b810b79767fc587367774a5a52c75
siyu.duan <siyu.duan@harperjames.co.uk>
2025-10-03 15:50:03 +0000

    added start icon conditionally (based on boolean value); and added a search filter

--- src/app/mock-recipes.ts
+++ src/app/mock-recipes.ts
@@ -6,6 +6,7 @@ export const MOCK_DRINKS: DrinkModel [] = [
     name: 'Mojito',
     description: 'A refreshing Cuban highball.',
     imgUrl: 'https://plus.unsplash.com/premium_photo-1721025986183-c33686ecf4d2?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    isFavorite: true,
     ingredients: [
       { name: 'White Rum', quantity: 2, unit: 'oz' },
       { name: 'Lime Juice', quantity: 1, unit: 'oz' },
@@ -19,6 +20,7 @@ export const MOCK_DRINKS: DrinkModel [] = [
     name: 'Old Fashioned',
     description: 'A classic cocktail of whiskey, bitters, sugar, and water.',
     imgUrl: 'https://images.unsplash.com/photo-1621873495884-845a939892d1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    isFavorite: false,
     ingredients: [
       { name: 'Bourbon or Rye Whiskey', quantity: 2, unit: 'oz' },
       { name: 'Angostura Bitters', quantity: 2, unit: 'dashes' },
--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,17 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DrinkModel } from '../models';
 import { MOCK_DRINKS } from '../mock-recipes';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RecipeDetail } from './recipe-detail/recipe-detail';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [CommonModule, RecipeDetail],
+  imports: [CommonModule, RecipeDetail, FormsModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css'
 })
 export class RecipeList {
   protected drink = signal<DrinkModel>({} as DrinkModel);
+  protected drinks = signal(MOCK_DRINKS);
+  protected searchTerm = signal('');
+  protected filteredDrinks = computed(() => {
+    const searchText = this.searchTerm().toLowerCase();
+    
+    if (!searchText) {
+      return this.drinks();
+    }
+    
+    return this.drinks().filter(drink => {
+      return drink.name.toLowerCase().includes(searchText);
+    })
+  });
 
   protected selectDrink(id: number): void {
     const selectedDrink = MOCK_DRINKS.find(d => d.id === id);
