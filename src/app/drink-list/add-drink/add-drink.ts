ddb9bf2be42db21796f3334a82897d3e59237d27
siyuduan.learning <siyuduan.learning@gmail.com>
2025-10-14 19:27:34 +0000

    fix some styling

--- src/app/drink-list/add-drink/add-drink.ts
+++ src/app/drink-list/add-drink/add-drink.ts
@@ -2,11 +2,11 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Drink } from '../../drink';
-import { MatButtonModule } from '@angular/material/button';
+// import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-add-drink',
-  imports: [ReactiveFormsModule, MatButtonModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-drink.html',
   styleUrl: './add-drink.css'
 })
